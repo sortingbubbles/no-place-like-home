6911fa84076bdd8208ddac1a6f5731ce436fbc99
Ani Chatsatrian <sortingbubbles@outlook.com>
2022-11-06 18:53:42 +0100

    feat: add  new cleaning schedule

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
+const compis = ['Amalia', 'Georgina', 'Ani']
+const places = ['cocina', 'sala', 'basura']
+
 function getCurrentWeekNumberOfTheMonth() {
   const currentDate = new Date();
   const startDate = new Date(currentDate.getFullYear(), 0, 1);
   const days = Math.floor((currentDate - startDate) / (24 * 60 * 60 * 1000));
-  const weekNumber = Math.ceil((currentDate.getDay() + 1 + days) / 7);
+  const weekNumber = Math.floor((currentDate.getDay() + 1 + days) / 7);
   return weekNumber
 }
 
-
 function whoHasToCleanThisWeek() {
     const weekNumber = getCurrentWeekNumberOfTheMonth()
-    if (weekNumber % 2 === 1) {
-        document.getElementById('clean-lady').innerText = 'Ani'
-    } else {
-        document.getElementById('clean-lady').innerText = 'Amalia'
+    const assignments = [places]
+    for (let index = 1; index <= 52; index++) {
+        const firstItem = assignments[index-1][0]
+        const middleItem = assignments[index-1][1]
+        const lastItem = assignments[index-1][2]
+        assignments[index] = [middleItem, lastItem, firstItem]
     }
+    document.getElementById(assignments[weekNumber][0]).innerText = compis[0]
+    document.getElementById(assignments[weekNumber][1]).innerText = compis[1]
+    document.getElementById(assignments[weekNumber][2]).innerText = compis[2]
 }
